5d963c19c5d5070f84d97257c3945bcbedf4d579
Brenner Heintz <brenner.heintz@gmail.com>
2018-12-12 12:44:45 -0800

    Changed batch_rename.py

--- scripts/batch_rename.py
+++ scripts/batch_rename.py
@@ -6,8 +6,6 @@ path_1 = ['/Users/brenner/project_kojak/frames/drawings'] * 6
 path_2 = ['/Users/brenner/project_kojak/frames/silhouettes'] * 6
 
 prefixes = ['C_', 'fist_', 'L_', 'okay_', 'palm_', 'peace_']
-drawing_prefixes = [f'drawing_{prefix}' for prefix in prefixes]
-new_prefixes = {'drawing_C_': 'C_', 'drawing_fist_': 'fist_', 'drawing_L_':'L_', 'drawing_okay_':'okay_', 'drawing_palm_': 'palm_', 'drawing_peace_': 'peace_'}
 
 def rename_strings(path, leading_string):
 	os.chdir(path)
@@ -16,9 +14,8 @@ def rename_strings(path, leading_string):
 	for f in os.listdir(path):
 		if f.startswith(leading_string):
 			counter += 1
-			new_prefix = new_prefixes[leading_string]
 			formatted_counter = str(counter).zfill(3)
-			os.rename(f, f.replace(f, f'{new_prefix}{formatted_counter}' + '.jpg'))
+			os.rename(f, f.replace(f, f'{leading_string}{formatted_counter}' + '_.jpg'))
 
 
 	print(f'Counted {counter} files.')
@@ -33,13 +30,11 @@ def count_images(path):
 	print(counts.items())
 
 
-print(count_images(path_1[0]))
-
 # # ***CALLS TO RENAME ALL FILES BELOW***
 # for tup in zip(path_2, prefixes):
 # 	rename_strings(tup[0], tup[1])
 #
-for tup in zip(path_1, drawing_prefixes):
-	rename_strings(tup[0], tup[1])
+# for tup in zip(path_1, prefixes):
+# 	rename_strings(tup[0], tup[1])
 
 print(count_images(path_1[0]))
